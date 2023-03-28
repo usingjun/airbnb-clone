3f25db156bea4411b89573c1617610c729ec8bb6
usingjun <97494494+usingjun@users.noreply.github.com>
2023-03-29 02:00:25 +0900

    amenities pages

--- rooms/urls.py
+++ rooms/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.see_all_rooms),
-    path("<int:room_pk>", views.see_one_room),
+    path("", views.Rooms.as_view()),
+    path("<int:pk>", views.RoomDetail.as_view()),
+    path("<int:pk>/reviews", views.RoomReviews.as_view()),
+    path("<int:pk>/amenities", views.RoomAmenities.as_view()),
+    path("amenities/", views.Amenities.as_view()),
+    path("amenities/<int:pk>", views.AmenityDetail.as_view()),
 ]
